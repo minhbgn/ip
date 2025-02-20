4f12a5d01708c2cc22b3aeb43079d5447192f2a8
minhbgn <nhatminh003@e.ntu.edu.sg>
2025-02-20 19:08:08 +0800

    Add branch-Level-5

--- src/main/java/item/Deadline.java
+++ src/main/java/item/Deadline.java
@@ -1,4 +1,4 @@
-package Item;
+package item;
 
 public class Deadline extends Item{
 
---
+++ src/main/java/main/Roboast.java
@@ -0,0 +1,57 @@
+package main;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import exception.RoboastException;
+import item.Item;
+import item.Todo;
+import item.Event;
+import item.Deadline;
+
+public class Roboast {
+    private static final String BOT_NAME = "Roboast";
+    private static final String LINE = "_".repeat(50);
+    private static final String[] COMMAND_LIST = {"list","mark","unmark","todo","deadline","event","deleteAll"};
+
+    private final ArrayList<Item> itemList = new ArrayList<Item>();
+
+    public void start() {
+        printHello();
+        itemManage();
+        printGoodbye();
+    }
+
+    public void printHello(){
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + BOT_NAME + "!");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    public void printGoodbye(){
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    public void itemManage(){
+
+        ItemManage itemManager = new ItemManage(itemList);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while (!input.equals("bye")){
+            try{
+                itemManager.action(input);
+            }
+            catch(RoboastException e){
+                System.out.println(e.getMessage());
+                itemManager.showCommandError();
+            }
+            input = sc.nextLine();
+        }
+
+    }
+
+}
